3a72a045ca6084a2513a3750a48250c6fb14d355
redpeacock78 <scarletpeacock78@gmail.com>
2023-07-13 02:09:35 +0900

    fix: Fix src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,12 @@ const app = new Hono();
 
 await Db.createShorterUrlTable();
 
-app.get("/", (c) => c.text("OK!"));
-app.post("/links", async (c) => {
+app.get("/", (c): Response => c.text("OK!"));
+app.post("/links", async (c): Promise<void | Response> => {
   const { url } = await c.req.json<{ url: string }>();
   if (!url) return c.text("Missing Url!", 400);
   try {
-    const getKey = await Db.getKey(url);
+    const getKey: Db.getKeyType = await Db.getKey(url);
     if (getKey.length === 0) {
       let id: string;
       while (true) {
@@ -21,24 +21,26 @@ app.post("/links", async (c) => {
       const key: string = Key.encode62(
         BigInt([...(id as string)].reverse().join(""))
       );
-      const setResult = await Db.setShorterUrl({
+      const setResult: Db.setResultType = await Db.setShorterUrl({
         id: BigInt(id),
         key: key,
         origin_url: url,
       });
       return c.json({ key: setResult[0].key, url: url });
     } else {
-      const originUrl = await Db.getOriginUrl(getKey[0].key);
+      const originUrl: Db.getOriginUrlType = await Db.getOriginUrl(
+        getKey[0].key
+      );
       return c.json({ key: getKey[0].key, url: originUrl[0].origin_url });
     }
   } catch {
     return c.status(500);
   }
 });
-app.get("/:key", async (c) => {
-  const key = c.req.param("key");
+app.get("/:key", async (c): Promise<void | Response> => {
+  const key: string = c.req.param("key");
   try {
-    const originUrl = await Db.getOriginUrl(key);
+    const originUrl: Db.getOriginUrlType = await Db.getOriginUrl(key);
     const url: string = originUrl[0].origin_url;
     if (!url) return c.notFound();
     return c.redirect(url, 301);
