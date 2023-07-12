2975d6188dfca241f2fad8279ddf1183280ce77c
redpeacock78 <scarletpeacock78@gmail.com>
2023-07-13 02:12:20 +0900

    fix: Fix src/libs/index.ts

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,3 +1,3 @@
-export { default as Snowflake } from "./snowflake.ts";
-export { default as Key } from "./key.ts";
-export { default as Db } from "./db.ts";
+export { default as Snowflake } from "@libs/snowflake.ts";
+export { default as Key } from "@libs/key.ts";
+export { default as Db } from "@libs/db.ts";
