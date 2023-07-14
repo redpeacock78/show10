705a0be9113e798ea1c779eeb6bc28a59a72177d
redpeacock78 <scarletpeacock78@gmail.com>
2023-07-14 19:25:02 +0900

    fix: Fix src/libs/isUrl.ts

--- src/libs/isUrl.ts
+++ src/libs/isUrl.ts
@@ -1,5 +1,7 @@
 export const isUrl = (url: string): boolean => {
   try {
+    if (url.match(/^https?:\/\/[\w!\?\+\-_~=;\.,\*&@#\$%\(\)'\[\]]+/) === null)
+      return false;
     new URL(url);
     return true;
   } catch {
