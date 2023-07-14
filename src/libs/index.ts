d0e62e19a09b47c00b1f0ec50cd95598c35eae7b
redpeacock78 <scarletpeacock78@gmail.com>
2023-07-14 12:01:26 +0900

    fix: Fix src/libs/index.ts

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,4 +1,5 @@
 export { Db } from "@libs/db.ts";
+export { Id } from "@libs/id.ts";
 export { Key } from "@libs/key.ts";
 export { isUrl } from "@libs/isUrl.ts";
 export { Snowflake } from "@libs/snowflake.ts";
