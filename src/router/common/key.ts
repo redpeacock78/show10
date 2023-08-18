7dcf0260c2fbc993c08603df1b1d7318486a785b
redpeacock78 <scarletpeacock78@gmail.com>
2023-08-18 10:04:14 +0900

    fix: Fix src/router/common/key.ts

--- src/router/common/key.ts
+++ src/router/common/key.ts
@@ -1,10 +1,11 @@
 import { Hono } from "hono";
-import { Db } from "libs";
+import { Db, isKey } from "libs";
 
 const key = new Hono();
 
 key.get("/:key", async (c): Promise<void | Response> => {
   const key: string = c.req.param("key");
+  if (!isKey(key)) return c.status(500);
   try {
     const originUrl: Db.getOriginUrlType = await Db.getOriginUrl(key);
     const url: string = originUrl[0].origin_url;
