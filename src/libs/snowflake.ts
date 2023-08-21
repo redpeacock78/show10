4f134e81f3ecebb262e4323720d9879dbe058267
redpeacock78 <scarletpeacock78@gmail.com>
2023-08-21 09:44:13 +0900

    feat: Change src/libs/snowflake.ts

--- src/libs/snowflake.ts
+++ src/libs/snowflake.ts
@@ -4,6 +4,7 @@
  * Sbowflakeに関する名前空間
  */
 export namespace Snowflake {
+  const baseTime: number = Date.UTC(2023, 6, 1, 0, 0, 0, 0);
   /**
    * Snowflakeを使用し一意な数値を生成する(基準時刻は2023/07/01 00:00:00.000 UTC)
    * @param {number} machineId 一意なMachine ID
@@ -14,7 +15,7 @@ export namespace Snowflake {
       let seq: bigint = BigInt(0);
       const shiftLeftTime: bigint = BigInt(22);
       const machine: bigint = BigInt(machineId) << BigInt(12);
-      const startingTime: bigint = BigInt(Date.UTC(2023, 6, 1, 0, 0, 0, 0));
+      const startingTime: bigint = BigInt(baseTime);
       let lastTime: bigint = BigInt(Date.now()) - startingTime;
       while (true) {
         try {
@@ -31,4 +32,16 @@ export namespace Snowflake {
         }
       }
     };
+  /**
+   * Snowflake生成された一意な値を生成された日付に戻す
+   * @param {number} id Snowflake生成された一意な値
+   * @returns 生成された日付
+   */
+  export const decode = (id: string): Date => {
+    const bitId: string = BigInt(id).toString(2);
+    const rm22BitId: string = bitId.slice(0, bitId.length - 22);
+    const result: Date = new Date(parseInt(rm22BitId, 2) + baseTime);
+    if (Number.isNaN(result.getTime())) new Error();
+    return result;
+  };
 }
