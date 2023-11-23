dfab1b43888df75d2def760ab10e92c175d5157f
redpeacock78 <scarletpeacock78@gmail.com>
2023-11-24 05:07:49 +0900

    chore: Change src/router/api/shorter.ts

--- src/router/api/shorter.ts
+++ src/router/api/shorter.ts
@@ -1,6 +1,6 @@
 // deno-lint-ignore-file no-explicit-any
 import { Hono } from "hono";
-import { Db, Id, Key, isUrl, MachineId } from "libs";
+import { isUrl, Shorter } from "libs";
 
 const shorter = new Hono();
 
@@ -9,22 +9,8 @@ shorter.post("/shorter", async (c: any): Promise<void | Response> => {
   if (!url) return c.text("Missing Url!", 400);
   if (!isUrl(url)) return c.text("Invalid Url!", 400);
   try {
-    const getKey: Db.getKeyType = await Db.getKey(url);
-    if (getKey.length === 0) {
-      const id: string = await Id.generate(MachineId.generate());
-      const key: string = Key.encode62(BigInt([...id].reverse().join("")));
-      const setResult: Db.setResultType = await Db.setShorterUrl({
-        id: BigInt(id),
-        key: key,
-        origin_url: url,
-      });
-      return c.json({ key: setResult[0].key, url: url });
-    } else {
-      const originUrl: Db.getOriginUrlType = await Db.getOriginUrl(
-        getKey[0].key
-      );
-      return c.json({ key: getKey[0].key, url: originUrl[0].origin_url });
-    }
+    const result = await Shorter.generater(url);
+    return c.json(result);
   } catch {
     return c.status(500);
   }
