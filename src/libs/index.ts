827b976fe16392594adc1c5447d6866cdb4963dc
redpeacock78 <scarletpeacock78@gmail.com>
2023-11-24 05:07:35 +0900

    chore: Change src/libs/index.ts

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -3,6 +3,7 @@ export { Id } from "@libs/id.ts";
 export { Key } from "@libs/key.ts";
 export { isKey } from "@libs/isKey.ts";
 export { isUrl } from "@libs/isUrl.ts";
+export { Shorter } from "@libs/shorter.ts";
 export { Netinfo } from "@libs/netInfo.ts";
 export { MachineId } from "@libs/machineId.ts";
 export { Snowflake } from "@libs/snowflake.ts";
