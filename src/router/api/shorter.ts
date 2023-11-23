7854976c8ef0cb524b738fab34dfe5aca38d3184
redpeacock78 <scarletpeacock78@gmail.com>
2023-11-24 04:18:06 +0900

    chore: Change src/router/api/shorter.ts

--- src/router/api/shorter.ts
+++ src/router/api/shorter.ts
@@ -1,16 +1,17 @@
+// deno-lint-ignore-file no-explicit-any
 import { Hono } from "hono";
-import { Db, Id, Key, isUrl } from "libs";
+import { Db, Id, Key, isUrl, MachineId } from "libs";
 
 const shorter = new Hono();
 
-shorter.post("/shorter", async (c): Promise<void | Response> => {
-  const { url } = await c.req.json<{ url: string }>();
+shorter.post("/shorter", async (c: any): Promise<void | Response> => {
+  const { url } = await c.req.json();
   if (!url) return c.text("Missing Url!", 400);
   if (!isUrl(url)) return c.text("Invalid Url!", 400);
   try {
     const getKey: Db.getKeyType = await Db.getKey(url);
     if (getKey.length === 0) {
-      const id: string = await Id.generate(0);
+      const id: string = await Id.generate(MachineId.generate());
       const key: string = Key.encode62(BigInt([...id].reverse().join("")));
       const setResult: Db.setResultType = await Db.setShorterUrl({
         id: BigInt(id),
