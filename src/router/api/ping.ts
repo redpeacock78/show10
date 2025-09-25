2991810908bd5f4f8f0588eda608748ac11b2bc1
redpeacock78 <scarletpeacock78@gmail.com>
2025-09-25 17:13:08 +0900

    fix: Fix src/router/api/ping.ts

--- src/router/api/ping.ts
+++ src/router/api/ping.ts
@@ -1,8 +1,15 @@
 // deno-lint-ignore-file no-explicit-any
 import { Hono } from "hono";
+import { Db } from "libs";
 
 const ping = new Hono();
 
-ping.get("/ping", (c: any): Response => c.text("OK!"));
+ping.get(
+  "/ping",
+  async (c: any): Promise<Response> =>
+    await Db.ping()
+      .then(() => c.text("OK!"))
+      .catch(() => c.text("DB Connection Error!", 500))
+);
 
 export default ping;
