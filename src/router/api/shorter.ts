a92f8248eb63b87ef354371d9e4ea608515f4818
redpeacock78 <scarletpeacock78@gmail.com>
2025-09-27 17:07:22 +0900

    fix: Fix src/router/api/shorter.ts

--- src/router/api/shorter.ts
+++ src/router/api/shorter.ts
@@ -1,19 +1,23 @@
 // deno-lint-ignore-file no-explicit-any
 import { Hono } from "hono";
-import { isUrl, Shorter } from "libs";
+import { isUrl, Shorter, Constants } from "libs";
 
 const shorter = new Hono();
 
-shorter.post("/shorter", async (c: any): Promise<void | Response> => {
-  const { url } = await c.req.json();
-  if (!url) return c.text("Missing Url!", 400);
-  if (!isUrl(url)) return c.text("Invalid Url!", 400);
-  try {
-    const result = await Shorter.generater(url);
-    return c.json(result);
-  } catch {
-    return c.status(500);
+shorter.post(
+  `${Constants.SHORTER_PATH}`,
+  async (c: any): Promise<void | Response> => {
+    try {
+      const { url } = await c.req.json();
+      if (!url) return c.text("Missing Url!", 400);
+      if (!isUrl(url)) return c.text("Invalid Url!", 400);
+      return await Shorter.generater(url)
+        .then((result) => c.json(result))
+        .catch(() => c.status(500));
+    } catch {
+      return c.text("Invalid Request!", 400);
+    }
   }
-});
+);
 
 export default shorter;
