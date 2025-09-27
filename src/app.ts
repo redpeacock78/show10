efd9a10b6fb3a6ba8b8a6b69d4653cd9989d29c0
redpeacock78 <scarletpeacock78@gmail.com>
2025-09-27 17:08:29 +0900

    fix: Fix src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import { Hono } from "hono";
 import { serve } from "serve";
-import { Db } from "libs";
+import { Db, Constants } from "libs";
 import { api, common } from "router";
 import app from "./app.tsx";
 
 const server = new Hono();
 
-await Db.createShorterUrlTable();
-
-server.route("/", api);
-server.route("/", app);
-server.route("/", common);
-
-serve(server.fetch);
+await Db.createShorterUrlTable()
+  .then(() => {
+    server.route(`${Constants.ROOT_PATH}`, api);
+    server.route(`${Constants.ROOT_PATH}`, app);
+    server.route(`${Constants.ROOT_PATH}`, common);
+    serve(server.fetch);
+  })
+  .catch(() => {
+    console.error("Failed to connect to the database.");
+  });
