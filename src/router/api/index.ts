51dc3e851f8bc8352319fd55ba6677f7541ab507
redpeacock78 <scarletpeacock78@gmail.com>
2025-09-27 17:37:07 +0900

    fix: Fix src/router/api/index.ts

--- src/router/api/index.ts
+++ src/router/api/index.ts
@@ -4,7 +4,7 @@ import { default as ping } from "@router/api/ping.ts";
 import { default as shorter } from "@router/api/shorter.ts";
 
 const api = new Hono().basePath(
-  `${Constants.BASE_PATH}${Constants.ROOT_PATH}${Constants.API_VERSION}`
+  `${Constants.API_PATH}${Constants.ROOT_PATH}${Constants.API_VERSION}`
 );
 
 api.route(`${Constants.ROOT_PATH}`, ping);
