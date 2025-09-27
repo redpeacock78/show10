2426fff967bc3e10281611a66c7d4a4bd7089fdd
redpeacock78 <scarletpeacock78@gmail.com>
2025-09-27 17:17:42 +0900

    fix: Fix src/router/common/key.ts

--- src/router/common/key.ts
+++ src/router/common/key.ts
@@ -1,20 +1,27 @@
 // deno-lint-ignore-file no-explicit-any
 import { Hono } from "hono";
-import { Db, isKey } from "libs";
+import { Db, isKey, Constants } from "libs";
 
 const key = new Hono();
 
-key.get("/:key", async (c: any): Promise<void | Response> => {
-  const key: string = c.req.param("key");
-  if (!isKey(key)) return c.text("Invalid Url!", 400);
-  try {
-    const originUrl: Db.getOriginUrlType = await Db.getOriginUrl(key);
-    const url: string = originUrl[0].origin_url;
-    if (!url) return c.notFound();
-    return c.redirect(url, 301);
-  } catch {
-    return c.status(500);
+key.get(
+  `${Constants.ROOT_PATH}:key`,
+  async (c: any): Promise<void | Response> => {
+    try {
+      const key: string = c.req.param("key");
+      if (!isKey(key)) return c.text("Invalid Url!", 400);
+      return await Db.getOriginUrl(key)
+        .then((originUrl) => {
+          if (!originUrl.length) return c.notFound();
+          const url: string = originUrl[0].origin_url;
+          if (!url) return c.notFound();
+          return c.redirect(url, 301);
+        })
+        .catch(() => c.status(500));
+    } catch {
+      return c.text("Invalid Request!", 400);
+    }
   }
-});
+);
 
 export default key;
