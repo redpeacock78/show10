742c45f001d182ba49725ebbb8a1225f58185b00
redpeacock78 <scarletpeacock78@gmail.com>
2025-09-27 17:18:15 +0900

    fix: Fix src/router/common/index.ts

--- src/router/common/index.ts
+++ src/router/common/index.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono";
+import { Constants } from "libs";
 import { default as key } from "@router/common/key.ts";
 
 const common = new Hono();
 
-common.route("/", key);
+common.route(`${Constants.ROOT_PATH}`, key);
 
 export default common;
